a7925b4cfa52c9ffbce990c64fc12efcb4e137a1
Yuvraj171 <97309229+Yuvraj171@users.noreply.github.com>
2022-01-26 16:39:45 +0530

    Add files via upload

---
+++ Assign2_21103009.py
@@ -0,0 +1,99 @@
+# Question 1
+inp_str = str(input('Enter the string:'))
+# inp_str is a variable used for the user to enter a string
+
+print('length of string is:', len(inp_str))
+print('Reverse of string is:', inp_str[::-1])
+
+slice_str = inp_str[9:26]
+# slice_str is a variable used to slice the inp_str(string entered by the user)
+
+print(slice_str)
+new_str = inp_str.replace('a case sensitive', 'object oriented')
+# new_str is a variable used for replacing replacing existing part of the string with a new part
+
+print('index of substring a', inp_str.index('a'))
+print(inp_str.replace(' ', ''))
+
+
+
+
+# Question 2
+
+# name variable is used for the user to enter his name
+name = str(input('Enter your name:'))
+
+# sid variable is used for the user to enter his SID
+sid = int(input('Enter you sid:'))
+
+# dept variable is used for the user to enter his department
+dept = str(input('Enter your department:'))
+
+# cgpa variable is used for the user to enter his CGPA
+cgpa = float(input('Enter your CGPA:'))
+print('Hey,', name, 'Here!\n' 
+      'My SID is ', sid, '\n'
+      'I am from', dept, 'and my CGPA is:', cgpa)
+
+
+
+# Question 3
+
+a = 56
+# a is a variable which assigned a value of 56
+
+b = 10
+# a is a variable which assigned a value of 56
+
+print('a&b:', a & b)
+print('a|b:', a | b)
+print('a^b:', a ^ b)
+print('Left shift "a" with 2 bits (a<<2): ', a << 2, ',and Left shift "b" with 4 bits (b<<4): ', b << 4)
+print('Right shift "a" with 2 bits (a>>2):', a >> 2, ',and Right shift "b" with 4 bits (b>>4):', b >> 4)
+
+
+
+# Question 4
+
+list = []
+for i in range(3):
+    num = float(input("Enter number : "))
+    list.append(num)
+
+list.sort(reverse=True)
+print("The greatest value is:", list[0])
+
+
+
+# Question 5
+
+inp_string = str(input('Enter a string:'))
+if 'name' in inp_string:
+    print('Yes')
+else:
+    print('No')
+
+
+
+# Question 6
+
+a1 = float(input('Enter side 1 of a triangle:'))
+# a1 is the variable used for the user to enter the first side of a triangle
+
+a2 = float(input('Enter side 2 of a triangle:'))
+# a2 is the variable used for the user to enter the second side of a triangle
+
+a3 = float(input('Enter side 3 of a triangle:'))
+# a3 is the variable used for the user to enter the third side of a triangle
+
+if a1 > (a2 + a3):
+    print('No')
+elif a3 > (a2 + a1):
+    print('No')
+elif a2 > (a1 + a3):
+    print('No')
+else:
+    print('Yes')
+
+
+
---
+++ Assign2_21103009_output.py
@@ -0,0 +1,32 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\ASUS\Dropbox\My PC (LAPTOP-15ICAJTU)\Desktop\Intro to computing\LAB\LAB 2\Assign2_21103009.py
+Enter the string:Python is a case sensitive language
+length of string is: 35
+Reverse of string is: egaugnal evitisnes esac a si nohtyP
+ a case sensitive
+index of substring a 10
+Pythonisacasesensitivelanguage
+Enter your name:Yuvraj Singh
+Enter you sid:21103009
+Enter your department:CSE
+Enter your CGPA:9.9
+Hey, Yuvraj Singh Here!
+My SID is  21103009 
+I am from CSE and my CGPA is: 9.9
+a&b: 8
+a|b: 58
+a^b: 50
+Left shift "a" with 2 bits (a<<2):  224 ,and Left shift "b" with 4 bits (b<<4):  160
+Right shift "a" with 2 bits (a>>2): 14 ,and Right shift "b" with 4 bits (b>>4): 0
+Enter number : 2
+Enter number : 4
+Enter number : -1
+The greatest value is: 4.0
+Enter a string:name
+Yes
+Enter side 1 of a triangle:4
+Enter side 2 of a triangle:2
+Enter side 3 of a triangle:1
+No
