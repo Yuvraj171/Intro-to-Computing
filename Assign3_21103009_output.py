b897f8b5c0c06352505f3addaca00a1f45f7d37b
Yuvraj171 <97309229+Yuvraj171@users.noreply.github.com>
2022-02-14 22:32:39 +0530

    Add files via upload

---
+++ Assign3_21103009_output.py
@@ -0,0 +1,99 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=== RESTART: C:\Users\ASUS\Dropbox\My PC (LAPTOP-15ICAJTU)\Desktop\Assign3.py ==
+Enter a string aigbiabai
+{' ': 1, 'A': 3, 'I': 3, 'G': 1, 'B': 2}
+Enter a month : 5
+Enter a day :13
+Enter an year :2002
+Date : 13 / 5 / 2002
+Next Date : 14 / 5 / 2002
+Done!
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81), (10, 100)]
+Done!
+Give a number between 4 and 10 including them-4
+Performance=Poor
+Letter Grade=D
+Done!
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+Done!
+----------------(a)-------------------
+Give the sid (a number):4
+Enter your name:a
+give a letter y or n:y
+Give the sid (a number):66
+Enter your name:tt
+give a letter y or n:y
+Give the sid (a number):69
+Enter your name:tata
+give a letter y or n:y
+Give the sid (a number):151
+Enter your name:coa
+give a letter y or n:y
+Give the sid (a number):87
+Enter your name:yo
+give a letter y or n:n
+The required dictionary-- {4: 'a', 66: 'tt', 69: 'tata', 151: 'coa', 87: 'yo'}
+The list will be used in further parts--
+The required list-- [(4, 'a'), (66, 'tt'), (69, 'tata'), (151, 'coa'), (87, 'yo')]
+Done!
+----------------(b)------------------------
+The dictionary we had-
+{4: 'a', 66: 'tt', 69: 'tata', 151: 'coa', 87: 'yo'}
+now we have exchanged the key value pair but its not sorted-
+{'a': 4, 'tt': 66, 'tata': 69, 'coa': 151, 'yo': 87}
+The unsorted list--
+[('a', 4), ('tt', 66), ('tata', 69), ('coa', 151), ('yo', 87)]
+The sorted list--
+[('a', 4), ('coa', 151), ('tata', 69), ('tt', 66), ('yo', 87)]
+The sorted dictionary-
+{'a': 4, 'coa': 151, 'tata': 69, 'tt': 66, 'yo': 87}
+This is the dictionary sorted on the basis of name-
+{4: 'a', 151: 'coa', 69: 'tata', 66: 'tt', 87: 'yo'}
+Done!
+-----------------(c)-----------------
+The list we made before will be used now-
+[(4, 'a'), (66, 'tt'), (69, 'tata'), (151, 'coa'), (87, 'yo')]
+now the list is sorted-
+[(4, 'a'), (66, 'tt'), (69, 'tata'), (87, 'yo'), (151, 'coa')]
+sorted dictionary based on sid- {4: 'a', 66: 'tt', 69: 'tata', 87: 'yo', 151: 'coa'}
+Done!
+-----------------(d)-----------------
+Please enter one of the sids you entered before-151
+The name of the student with the entered sid is-
+coa
+Done!
+Give a number-5
+Give a number-6
+[5, 6, 11]
+Give y to continue and n to stop going further-y
+[5, 6, 11, 17]
+Give y to continue and n to stop going further-y
+[5, 6, 11, 17, 28]
+Give y to continue and n to stop going further-4
+Now we got a list having a fibonacci series-
+[5, 6, 11, 17, 28]
+Average of the sequence-
+13.4
+Done!
+-------------(a)------------
+{1, 3, 5, 6, 8}
+Done!
+-------------(b)------------
+{3, 6, 8, 9, 13, 17}
+Done!
+-------------(c)------------
+{1, 2, 4, 5}
+Done!
+-------------(d)------------
+{6, 7, 8, 9, 10}
+Done!
+-------------(e)------------
+{10, 7}
+Done!
