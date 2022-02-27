a69ef1982bc366e4efe105e3a8e3c6ff3b6d3f94
Yuvraj171 <97309229+Yuvraj171@users.noreply.github.com>
2022-02-27 22:04:54 +0530

    Add files via upload

---
+++ Assign4_21103009_Output.py
@@ -0,0 +1,65 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\ASUS\Dropbox\My PC (LAPTOP-15ICAJTU)\Desktop\Intro to computing\LAB\LAB 4\Assign4_21103009.py
+---------OUTPUT-1---------
+shifted disk 1 selected the rod A destination of rod C
+shifted disk 2 selected the rod A destination of rod B
+shifted disk 1 selected the rod C destination of rod B
+shifted disk 3 selected the rod A destination of rod C
+shifted disk 1 selected the rod B destination of rod A
+shifted disk 2 selected the rod B destination of rod C
+shifted disk 1 selected the rod A destination of rod C
+
+
+---------OUTPUT-2---------
+Enter the size of pascals triangle you want: 7
+PASCAL TRIANGLE USING LOOPS
+        1 
+       1 1 
+      1 2 1 
+     1 3 3 1 
+    1 4 6 4 1 
+   1 5 10 10 5 1 
+  1 6 15 20 15 6 1 
+
+
+
+PASCAL TRIANGLE USING RECURSIONS
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+ 1 6 15 20 15 6 1 
+
+
+---------OUTPUT-3---------
+enter first no.:14
+enter second no.:7
+part-(a):Checking whether the quotient and remainder is a callable value or not
+False
+False
+(b) The reminder is zero
+(c) Filtered out numbers that are greater than 4 are : [6, 5, 7, 5, 8, 6]
+(d) Set: {8, 5, 6, 7}
+(e) Immutable set: frozenset({8, 5, 6, 7})
+(f) Hash value of the maximum value from the above set: 8
+
+
+---------OUTPUT-4---------
+Object created
+The name of the student it Yuvraj and SID is 21103009.
+Object destroyed
+
+
+---------OUTPUT-5---------
+(a)The updated salary of Mehak is 70000 
+(b)Record of Viren deleted
+
+---------OUTPUT-6---------
+Enter the word: honesty
+Enter a word using the exact same letters to test your friendship: honesty
+Does the word makes sense?(y/Y or n/N )Y
+The friends pass the friendship test
